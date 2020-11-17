426a9fbea60a7a8b1e519dc25a7ee4cb0325cd0b
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-11-17 11:31:45 +0900

    Add Global css.
    Add Global css to set the basic body css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
 
+import { Global, css } from '@emotion/react';
+
+const GlobalStyles = css`
+body {
+  background-color: #fff;
+  margin : 0;
+  padding: 0;
+  position: relative;
+  width: 100vw;
+  height: 10vh;
+  overflow: hidden;
+}
+
+body::-webkit-scrollbar {
+display: none;
+}
+`;
+
 export default function App() {
   return (
-    <h1>Hello</h1>
+    <>
+      <Global styles={GlobalStyles} />
+      <h1>Hello</h1>
+    </>
   );
 }
