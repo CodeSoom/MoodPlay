2585627cebc38b54162f9369cde6b63ae7163d77
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-11-19 10:56:48 +0900

    Separate global css.
    Create assets folder and separate global css from App component.

--- webpack.config.js
+++ webpack.config.js
@@ -10,10 +10,15 @@ module.exports = {
         exclude: /node_modules/,
         use: 'babel-loader',
       },
+      {
+        test: /\.css?$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: ['.js', '.jsx', 'css'],
   },
   plugins: [
     new HtmlWebpackPlugin({
