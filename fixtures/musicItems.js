4936a7d959246d32994077cb86efa475e40898a5
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-11-29 14:08:32 +0900

    Add fixtures.
    Add musicItems and selectedMusic fixture in tests.

---
+++ fixtures/musicItems.js
@@ -0,0 +1,114 @@
+const musicItems = [
+  {
+    id: {
+      videoId: 'xxx1',
+    },
+    snippet: {
+      channelTitle: 'essential1',
+      description: 'description1',
+      title: 'title1',
+      thumbnails: {
+        medium: {
+          height: 120,
+          url: 'https://bbb.com/default.jpg',
+          width: 240,
+        },
+        default: {
+          height: 90,
+          url: 'https://bbb.com/default.jpg',
+          width: 120,
+        },
+      },
+    },
+  },
+  {
+    id: {
+      videoId: 'xxx2',
+    },
+    snippet: {
+      channelTitle: 'essential2',
+      description: 'description2',
+      title: 'title2',
+      thumbnails: {
+        medium: {
+          height: 120,
+          url: 'https://bbb.com/default.jpg',
+          width: 240,
+        },
+        default: {
+          height: 90,
+          url: 'https://bbb.com/default.jpg',
+          width: 120,
+        },
+      },
+    },
+  },
+  {
+    id: {
+      videoId: 'xxx3',
+    },
+    snippet: {
+      channelTitle: 'essential3',
+      description: 'description3',
+      title: 'title3',
+      thumbnails: {
+        medium: {
+          height: 120,
+          url: 'https://bbb.com/default.jpg',
+          width: 240,
+        },
+        default: {
+          height: 90,
+          url: 'https://bbb.com/default.jpg',
+          width: 120,
+        },
+      },
+    },
+  },
+  {
+    id: {
+      videoId: 'xxx4',
+    },
+    snippet: {
+      channelTitle: 'essential4',
+      description: 'description4',
+      title: 'title4',
+      thumbnails: {
+        medium: {
+          height: 120,
+          url: 'https://bbb.com/default.jpg',
+          width: 240,
+        },
+        default: {
+          height: 90,
+          url: 'https://bbb.com/default.jpg',
+          width: 120,
+        },
+      },
+    },
+  },
+  {
+    id: {
+      videoId: 'xxx5',
+    },
+    snippet: {
+      channelTitle: 'essential5',
+      description: 'description5',
+      title: 'title5',
+      thumbnails: {
+        medium: {
+          height: 120,
+          url: 'https://bbb.com/default.jpg',
+          width: 240,
+        },
+        default: {
+          height: 90,
+          url: 'https://bbb.com/default.jpg',
+          width: 120,
+        },
+      },
+    },
+  },
+];
+
+export default musicItems;
--- src/UpNextMusic.test.jsx
+++ src/UpNextMusic.test.jsx
@@ -4,122 +4,19 @@ import { render } from '@testing-library/react';
 
 import UpNextMusic from './UpNextMusic';
 
-test('UpNextMusic', () => {
-  const selectedMusic = {
-    id: {
-      videoId: 'xxx3',
-    },
-    snippet: {
-      channelTitle: 'essential3',
-      description: 'description3',
-      title: 'title3',
-      thumbnails: {
-        medium: {
-          url: 'https://bbb.com/default.jpg',
-        },
-        default: {
-          height: 90,
-          url: 'https://bbb.com/default.jpg',
-          width: 120,
-        },
-      },
-    },
-  };
+import MUSICITEMS from '../fixtures/musicItems';
 
-  const nowPlayingMusicItems = [
-    {
-      id: {
-        videoId: 'xxx1',
-      },
-      snippet: {
-        channelTitle: 'essential1',
-        description: 'description1',
-        title: 'title1',
-        thumbnails: {
-          default: {
-            height: 90,
-            url: 'https://bbb.com/default.jpg',
-            width: 120,
-          },
-        },
-      },
-    },
-    {
-      id: {
-        videoId: 'xxx2',
-      },
-      snippet: {
-        channelTitle: 'essential2',
-        description: 'description2',
-        title: 'title2',
-        thumbnails: {
-          default: {
-            height: 90,
-            url: 'https://bbb.com/default.jpg',
-            width: 120,
-          },
-        },
-      },
-    },
-    {
-      id: {
-        videoId: 'xxx3',
-      },
-      snippet: {
-        channelTitle: 'essential3',
-        description: 'description3',
-        title: 'title3',
-        thumbnails: {
-          default: {
-            height: 90,
-            url: 'https://bbb.com/default.jpg',
-            width: 120,
-          },
-        },
-      },
-    },
-    {
-      id: {
-        videoId: 'xxx4',
-      },
-      snippet: {
-        channelTitle: 'essential4',
-        description: 'description4',
-        title: 'title4',
-        thumbnails: {
-          default: {
-            height: 90,
-            url: 'https://bbb.com/default.jpg',
-            width: 120,
-          },
-        },
-      },
-    },
-    {
-      id: {
-        videoId: 'xxx5',
-      },
-      snippet: {
-        channelTitle: 'essential5',
-        description: 'description5',
-        title: 'title5',
-        thumbnails: {
-          default: {
-            height: 90,
-            url: 'https://bbb.com/default.jpg',
-            width: 120,
-          },
-        },
-      },
-    },
-  ];
+test('UpNextMusic', () => {
+  const nowPlayingMusicItems = MUSICITEMS;
+
+  const selectedMusic = MUSICITEMS[3];
 
   const { container } = render(<UpNextMusic
     selectedMusic={selectedMusic}
     nowPlayingMusicItems={nowPlayingMusicItems}
   />);
 
-  expect(container).toHaveTextContent('title4');
-  expect(container).toHaveTextContent('title5');
-  expect(container).toHaveTextContent('title1');
+  expect(container).toHaveTextContent(MUSICITEMS[4].snippet.title);
+  expect(container).toHaveTextContent(MUSICITEMS[0].snippet.title);
+  expect(container).toHaveTextContent(MUSICITEMS[1].snippet.title);
 });
