6d725ed9f85b77fadca4f7ce503f8704d10d1168
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-12-07 00:27:49 +0900

    Updates

--- src/sevices/storage.js
+++
@@ -1,9 +0,0 @@
-export function loadItem(key) {
-  const data = localStorage.getItem(key);
-
-  return JSON.parse(data);
-}
-
-export function saveItem(key, value) {
-  return localStorage.setItem(key, JSON.stringify(value));
-}
