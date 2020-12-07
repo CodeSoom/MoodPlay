6bf5c074f9249f8dbc14ab84d806183ee59f3733
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-12-07 11:54:25 +0900

    Add navigation bar style.
    Add navigation bar style and icons.

--- src/assets/images.js
+++ src/assets/images.js
@@ -4,10 +4,31 @@ import VolumeIcon from './images/icons/sound_on.png';
 import MuteIcon from './images/icons/sound_off.png';
 import StoreIcon from './images/icons/bookmark.png';
 
+import LogoIcon from './images/icons/Logo.png';
+import HomeIcon from './images/icons/home-i.png';
+import MoodControllerIcon from './images/icons/selet-mood-icon-i.png';
+import MyPlayIcon from './images/icons/myplaylist-i.png';
+import HomeIconHover from './images/icons/home-h.png';
+import MoodControllerIconHover from './images/icons/selet-mood-icon-h.png';
+import MyPlayIconHover from './images/icons/myplaylist-h.png';
+import HomeIconActive from './images/icons/home-a.png';
+import MoodControllerIconActive from './images/icons/selet-mood-icon-a.png';
+import MyPlayIconActive from './images/icons/myplaylist-a.png';
+
 export {
   PlayIcon,
   PauseIcon,
   VolumeIcon,
   MuteIcon,
   StoreIcon,
+  LogoIcon,
+  HomeIcon,
+  MoodControllerIcon,
+  MyPlayIcon,
+  HomeIconHover,
+  MoodControllerIconHover,
+  MyPlayIconHover,
+  HomeIconActive,
+  MoodControllerIconActive,
+  MyPlayIconActive,
 };
