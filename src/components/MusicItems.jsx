b4ad25bf71d64d228bcd80b5ea0a34194027474c
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-12-09 11:53:33 +0900

    Apply styles to MusicItems.

--- src/components/MusicItems.jsx
+++ src/components/MusicItems.jsx
@@ -2,50 +2,128 @@ import React from 'react';
 
 import styled from '@emotion/styled';
 
-const MusicWrap = styled.ul({
+const Wrap = styled.div({
+  marginTop: '8vh',
+  height: '58vh',
+});
+
+const MusicItemsTitle = styled.h3({
+  fontSize: '28px',
+  fontWeight: '900',
+});
+
+const MusicItemsWrap = styled.ul({
   display: 'flex',
   flexDirection: 'column',
-  height: '80%',
-  overflowY: 'scroll',
+  flexWrap: 'wrap',
+  width: '100%',
+  height: '50vh',
+  overflow: 'hidden',
 });
 
 const MusicItem = styled.li({
   display: 'flex',
   flexDirection: 'row',
-  justifyContent: 'center',
+  justifyContent: 'space-around',
   alignItems: 'center',
+  margin: '10px 35px',
   padding: '5px 10px',
-  margin: '10px',
-  border: '2px solid #f0f0f0',
-  borderRadius: '5px',
+  width: '447px',
+  height: '10vh',
+  borderRadius: '12px',
+  background: '#181818',
   cursor: 'pointer',
+
+  '&: hover': {
+    transform: 'translateY(-5px)',
+    boxShadow: ' 0px 8px 24px rgba(255, 255, 255, 0.2)',
+  },
+
+  '&: active': {
+    transform: 'translateY(2px)',
+    boxShadow: ' 8px 8px 24px rgba(255, 255, 255, 0.2)',
+  },
 });
 
-const MusicItems = React.memo(({ music, onClick }) => (
-  <MusicWrap>
-    {
-      music.map((musicItem) => {
-        const {
-          snippet: {
-            title, description, channelTitle, thumbnails,
-          },
-        } = musicItem;
+const Thumbnail = styled.div(({ url }) => ({
+  fontSize: '0',
+  width: '9.4vh',
+  height: '7vh',
+  borderRadius: '12px',
+  background: `url(${url}) no-repeat`,
+  backgroundSize: '100% 100%',
+  backgroundPosition: 'center',
+}));
 
-        return ((
-          <MusicItem
-            key={title}
-            onClick={() => onClick(musicItem, music)}
-          >
-            <img src={thumbnails.default.url} alt={description} />
-            <div>
-              <p>{title}</p>
-              <small>{channelTitle}</small>
-            </div>
-          </MusicItem>
-        ));
-      })
-    }
-  </MusicWrap>
-));
+const InfoBox = styled.div({
+  width: '70%',
+});
+
+const TitleWrap = styled.div({
+  width: '100%',
+  height: '3em',
+  overflow: 'hidden',
+});
+
+const Title = styled.p`
+  font-size: 16px;
+  font-weight: 700;
+  display: -webkit-box;
+  -webkit-line-clamp: 2; 
+  -webkit-box-orient: vertical;
+  height:  '3em';
+  lineHeight: '1.2em';
+  wordBreak: 'break-all';
+  whiteSpace: 'nowrap';
+  overflow: 'hidden';
+  text-overflow: 'ellipsis';
+  `;
+
+const ChannelTitle = styled.small({
+  color: '#999',
+});
+
+const MusicItems = React.memo(({
+  music,
+  selectedCategory,
+  onClick,
+}) => ((
+  <Wrap>
+    <MusicItemsTitle>
+      {selectedCategory}
+    </MusicItemsTitle>
+
+    <MusicItemsWrap>
+      {
+        music.map((musicItem) => {
+          const {
+            snippet: {
+              title, description, channelTitle, thumbnails,
+            },
+          } = musicItem;
+
+          return ((
+            <MusicItem
+              key={title}
+              onClick={() => onClick(musicItem, music)}
+            >
+              <Thumbnail
+                url={thumbnails.default.url}
+              >
+                {description}
+              </Thumbnail>
+              <InfoBox>
+                <TitleWrap>
+                  <Title>{title}</Title>
+                </TitleWrap>
+                <ChannelTitle>{channelTitle}</ChannelTitle>
+              </InfoBox>
+            </MusicItem>
+          ));
+        })
+      }
+    </MusicItemsWrap>
+  </Wrap>
+)));
 
 export default MusicItems;
