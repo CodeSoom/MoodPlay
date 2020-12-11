1a2e45f220f24f87157517a3b3def7a3473d526f
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-12-11 10:38:08 +0900

    Hide the home menu.
    Display the home menu after select moods.

--- src/components/NevigationBar.test.jsx
+++ src/components/NevigationBar.test.jsx
@@ -6,14 +6,38 @@ import { MemoryRouter } from 'react-router-dom';
 
 import NavigationBar from './NavigationBar';
 
-test('NavigationBar', () => {
-  const { container } = render(
-    <MemoryRouter>
-      <NavigationBar />
-    </MemoryRouter>,
-  );
+describe('NavigationBar', () => {
+  function renderNavigationBar(moodCategories) {
+    return render((
+      <MemoryRouter>
+        <NavigationBar
+          moodCategories={moodCategories}
+        />
+      </MemoryRouter>
+    ));
+  }
 
-  expect(container).toHaveTextContent('무드선택');
-  expect(container).toHaveTextContent('홈');
-  expect(container).toHaveTextContent('마이플레이');
+  context('with moodCategories', () => {
+    const moodCategories = [{}];
+
+    it('renders all menus', () => {
+      const { container } = renderNavigationBar(moodCategories);
+
+      expect(container).toHaveTextContent('무드선택');
+      expect(container).toHaveTextContent('홈');
+      expect(container).toHaveTextContent('마이플레이');
+    });
+  });
+
+  context('with empty moodCategories', () => {
+    const moodCategories = [];
+
+    it('renders menus without home', () => {
+      const { container } = renderNavigationBar(moodCategories);
+
+      expect(container).toHaveTextContent('무드선택');
+      expect(container).not.toHaveTextContent('홈');
+      expect(container).toHaveTextContent('마이플레이');
+    });
+  });
 });
