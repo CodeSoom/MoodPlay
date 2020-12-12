988e01c7157c0b5d18ffaffb9bb39bce1bdbabb6
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-12-12 23:44:05 +0900

    Change mood controller page styles.
    Add media query to mood controller page

--- src/pages/MoodControllerPage.jsx
+++ src/pages/MoodControllerPage.jsx
@@ -2,23 +2,34 @@ import React from 'react';
 
 import styled from '@emotion/styled';
 
+import facepaint from 'facepaint';
+
 import MoodControllerContainer from '../containers/MoodControllerContainer';
 
 import { BACKGROUND_COLOR_BLACK } from '../styles/constants';
 
-const Wrap = styled.div({
+const mq = facepaint([
+  '@media(min-width: 672px)',
+  '@media(min-width: 1000px)',
+]);
+
+const Wrap = styled.div(() => mq({
   position: 'fixed',
-  left: '234px',
+  left: [0, '120px', '12vw'],
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'center',
   alignItems: 'center',
-  width: 'calc(100% - 234px)',
+  width: ['100%', 'calc(100% - 120px)', 'calc(100% - 12vw)'],
   height: '100vh',
-  borderRadius: '0 48px 48px 0',
   background: BACKGROUND_COLOR_BLACK,
   color: '#fff',
-});
+
+  '& h1': {
+    fontSize: ['4.5vw', '2.2vw', '2.2vw'],
+    fontWeight: 'bold',
+  },
+}));
 
 export default function MoodControllerPage() {
   return (
